a4748ef67989fb4685a494a3947190d428aef1c8
MrChris-2025 <7rings@protonmail.com>
2026-09-01 13:56:58 -0400

    Update moviesources.js

--- moviesources.js
+++ moviesources.js
@@ -18,7 +18,7 @@ const serversList = [
     { id: 'oneembed', name: 'Server 13: 1Embed' }
 ];
 
-const noSandboxSources = ['vault', 'xpass', 'vidcore', 'oneembed', 'vidfast'];
+const noSandboxSources = ['vault', 'xpass', 'vid', 'oneembed', 'vidfast'];
 let activeServer = "vidcore";
 
 /**
