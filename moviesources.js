fab8f81b6ff03b3414ef9f7df80286b8e8f738b9
MrChris-2025 <7rings@protonmail.com>
2026-08-31 21:46:16 -0400

    Update moviesources.js

--- moviesources.js
+++ moviesources.js
@@ -14,7 +14,7 @@ const serversList = [
     { id: 'nextbox', name: 'Server 9: NextBox' },
     { id: 'peestream', name: 'Server 10: PeeStream' },
     { id: 'gaia', name: 'Server 11: Gaia' },
-    { id: 'fmov', name: 'Server 12: fmov' },
+    { id: 'flixeo', name: 'Server 12: flixeo' },
     { id: 'oneembed', name: 'Server 13: 1Embed' }
 ];
 
@@ -38,7 +38,7 @@ function getStreamUrl(server, type, id, season = 1, episode = 1) {
             case "nextbox": return `https://nextbox.uno/player/movie/${id}`;
             case "peestream": return `https://peestream.in/embed/?type=movie&tmdbId=${id}`;
             case "gaia": return `https://gaiaflix.live/watch/${id}?type=movie`;
-            case "fmov": return `https://fmov.my/embed/movie/{movie_id}`
+            case "flixeo": return `https://flixeo.tv/watch/movie/{id}`
             case "oneembed": return `https://1embed.cc/embed/movie/${id}`;
             default: return `https://player.videasy.net/movie/${id}?color=8834ec`;
         }
@@ -55,7 +55,7 @@ function getStreamUrl(server, type, id, season = 1, episode = 1) {
             case "nextbox": return `https://nextbox.uno/player/tv/${id}/${season}/${episode}`;
             case "peestream": return `https://peestream.in/embed/?type=show&tmdbId=${id}&season=${season}&episode=${episode}`;
             case "gaia": return `https://gaiaflix.live/watch/${id}?type=tv&s=${season}&e=${episode}`;
-            case "fmov": return `https://fmov.my/embed/tv/show_id/season/episode`; 
+            case "flixeo": return `https://flixeo.tv/watch/tv/{id}/season/episode`; 
             case "oneembed": return `https://1embed.cc/embed/tv/${id}/${season}/${episode}`;
             default: return `https://player.videasy.net/tv/${id}/${season}/${episode}?autoplayNextEpisode=true&autoPlay=true&nextEpisode=true&color=8834ec`;
         }
