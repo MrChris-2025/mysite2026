9c115541175447eee529360caf4a93201f4833a0
MrChris-2025 <7rings@protonmail.com>
2026-09-03 16:29:39 -0400

    Update moviesources.js

--- moviesources.js
+++ moviesources.js
@@ -21,7 +21,7 @@ const serversList = [
 ];
 
 const noSandboxSources = ['vault', 'xpass', 'vid', 'oneembed', 'vidfast'];
-let activeServer = "xpass";
+let activeServer = "";
 
 let isPlaying = true;
 let isSeeking = false;
