7ed6d5031ac0b87ab573aca562af7cf57716ea07
MrChris-2025 <7rings@protonmail.com>
2026-09-07 12:11:29 -0400

    Update endpoints.js

--- endpoints.js
+++ endpoints.js
@@ -1,9 +1,9 @@
-const SOURCES = [
-    { name: "Buffsports", url: "https://livelive24.com/buffsports.json" },
+const STREAM_ENDPOINTS = [
     { name: "Zlatans", url: "https://livelive24.com/test/processed_matches_prioritized.json" },
     { name: "Main", url: "https://livelive24.com/main.json" },
-    { name: "WWHD", url: "https://livelive24.com/test/processed_matches_other.json" },
+    { name: "WWHD ⚽ ", url: "https://livelive24.com/test/processed_matches_other.json" },
     { name: "CR7", url: "https://livelive24.com/streamedpk.json" },
     { name: "Pele", url: "https://livelive24.com/ppv.json" },
-    { name: "Zidane", url: "https://livelive24.com/onlive.json" }
+    { name: "Zidane", url: "https://livelive24.com/onlive.json" },
+    { name: "Buffsports", url: "https://livelive24.com/buffsports.json" }
 ];
