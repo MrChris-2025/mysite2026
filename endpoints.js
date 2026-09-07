8d29fe7fae8ee69475e63fb0452b340e1089131d
MrChris-2025 <7rings@protonmail.com>
2026-09-06 21:58:21 -0400

    Add REMOTE_SOURCES array with endpoint URLs

---
+++ endpoints.js
@@ -0,0 +1,10 @@
+// endpoints.js
+const REMOTE_SOURCES = [
+    { name: "Buffsports", url: "https://livelive24.com/buffsports.json" },
+    { name: "Zlatans", url: "https://livelive24.com/test/processed_matches_prioritized.json" },
+    { name: "Main", url: "https://livelive24.com/main.json" },
+    { name: "WWHD", url: "https://livelive24.com/test/processed_matches_other.json" },
+    { name: "CR7", url: "https://livelive24.com/streamedpk.json" },
+    { name: "Pele", url: "https://livelive24.com/ppv.json" },
+    { name: "Zidane", url: "https://livelive24.com/onlive.json" }
+];
