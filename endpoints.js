c800f6174faaae42e7aec81121dffd8357029f96
MrChris-2025 <7rings@protonmail.com>
2026-09-07 20:54:54 -0400

    Update endpoints.js

--- endpoints.js
+++ endpoints.js
@@ -1,9 +1,18 @@
-const API_ENDPOINTS = [
-    { name: "Zlatans", url: "https://livelive24.com/test/processed_matches_prioritized.json" },
-    { name: "Main", url: "https://livelive24.com/main.json" },
-    { name: "WWHD ⚽ ", url: "https://livelive24.com/test/processed_matches_other.json" },
-    { name: "CR7", url: "https://livelive24.com/streamedpk.json" },
-    { name: "Pele", url: "https://livelive24.com/ppv.json" },
-    { name: "Zidane", url: "https://livelive24.com/onlive.json" },
-    { name: "BuffSports", url: "https://livelive24.com/buffsports.json" }
+/**
+ * Stream API Sources and Base Endpoint Configurations
+ */
+
+const ENDPOINTS = [
+  { name: "Zlatans", url: "https://livelive24.com/test/processed_matches_prioritized.json" },
+  { name: "Main", url: "https://livelive24.com/main.json" },
+  { name: "WWHD ⚽", url: "https://livelive24.com/test/processed_matches_other.json" },
+  { name: "CR7", url: "https://livelive24.com/streamedpk.json" },
+  { name: "Pele", url: "https://livelive24.com/ppv.json" },
+  { name: "Zidane", url: "https://livelive24.com/onlive.json" },
+  { name: "Buffsports", url: "https://livelive24.com/buffsports.json" }
 ];
+
+const API_BASES = {
+  onsport: "https://api.onsport365.live/v5/matches/view?lang=en&id=",
+  sportplus: "https://api.sportplustv.live/v5/matches/index?sport_id="
+};
