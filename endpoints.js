a037492dbf3a22113c257a78856e706eb073d07f
MrChris-2025 <7rings@protonmail.com>
2026-09-07 20:38:39 -0400

    Update endpoints.js

--- endpoints.js
+++ endpoints.js
@@ -1,12 +1,9 @@
-const endpoints = [
+const API_ENDPOINTS = [
     { name: "Zlatans", url: "https://livelive24.com/test/processed_matches_prioritized.json" },
     { name: "Main", url: "https://livelive24.com/main.json" },
-    { name: "WWHD ⚽", url: "https://livelive24.com/test/processed_matches_other.json" },
+    { name: "WWHD ⚽ ", url: "https://livelive24.com/test/processed_matches_other.json" },
     { name: "CR7", url: "https://livelive24.com/streamedpk.json" },
     { name: "Pele", url: "https://livelive24.com/ppv.json" },
-    { name: "Zidane", url: "https://livelive24.com/onlive.json" }
+    { name: "Zidane", url: "https://livelive24.com/onlive.json" },
+    { name: "BuffSports", url: "https://livelive24.com/buffsports.json" }
 ];
-
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = endpoints;
-}
